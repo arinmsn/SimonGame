81b12ac0f0b1e3b6d70cbb61203be02009d46a6a
arinmsn <arinmsn@gmail.com>
2019-07-18 11:15:27 -0700

    App works.

--- JS/index.js
+++ JS/index.js
@@ -21,7 +21,7 @@ let on = false;
 let win;   
 
 const turnCounter = document.querySelector('#turn');
-const topLef = document.querySelector('#topleft');
+const topLeft = document.querySelector('#topleft');
 const  topRight = document.querySelector('#topright');
 const  bottomLeft = document.querySelector('#bottomleft');
 const  bottomRight = document.querySelector('#bottomright');
@@ -80,3 +80,187 @@ function play() {
     intervalId = setInterval(gameTurn, 800); // 800 ms
 }   // End of play() f(x)
 
+function gameTurn() {
+    // Player cannot click any button while
+    on = false;
+
+    // If the computer's turn is over, we'll clear the interval
+    if (flash == turn) {
+        clearInterval(intervalId);
+        compTurn = false;
+        clearColor();
+        on = true;
+    }
+
+    if (compTurn) {
+        clearColor();
+        // Will do something once after 
+        setTimeout(() => {
+            // flash starts from 0. 
+            if(order[flash] == 1) one();
+            if(order[flash] == 2) two();
+            if(order[flash] == 3) three();
+            if(order[flash] == 4) four();
+            flash++;
+        }, 200); 
+    }
+}
+
+function one() {
+    if (noise) {
+        let audio = document.getElementById("clip1");
+        audio.play();
+    }
+    noise = true; 
+    topLeft.style.backgroundColor = "lightgreen";
+}
+
+function two() {
+    if (noise) {
+        let audio = document.getElementById("clip2");
+        audio.play();
+    }
+    noise = true; 
+    topRight.style.backgroundColor = "tomato";
+}
+
+function three() {
+    if (noise) {
+        let audio = document.getElementById("clip3");
+        audio.play();
+    }
+    noise = true; 
+    bottomLeft.style.backgroundColor = "yellow";
+}
+
+function four() {
+    if (noise) {
+        let audio = document.getElementById("clip4");
+        audio.play();
+    }
+    noise = true; 
+    bottomRight.style.backgroundColor = "lightskyblue";
+}
+
+function clearColor() {
+    topLeft.style.backgroundColor = "darkgreen";
+    topRight.style.backgroundColor = "darkred";
+    bottomLeft.style.backgroundColor = "goldenrod";
+    bottomRight.style.backgroundColor = "darkblue";
+}
+
+function flashColor() {
+    topLeft.style.backgroundColor = "lightgreen";
+    topRight.style.backgroundColor = "tomato";
+    bottomLeft.style.backgroundColor = "yellow";
+    bottomRight.style.backgroundColor = "lightskyblue";
+}
+
+// Enabling user to click on sections
+
+topLeft.addEventListener('click', e => {
+    if (on) {
+        // Sections that user has clicked
+        playerOrder.push(1);
+        check();
+        one();
+        if (!win) {
+            setTimeout(() => {
+                clearColor();
+            }, 300);
+        }
+    }
+});
+
+topRight.addEventListener('click', e => {
+    if (on) {
+        // Sections that user has clicked
+        playerOrder.push(2);
+        check();
+        two();
+        if (!win) {
+            setTimeout(() => {
+                clearColor();
+            }, 300);
+        }
+    }
+});
+
+bottomLeft.addEventListener('click', e => {
+    if (on) {
+        // Sections that user has clicked
+        playerOrder.push(3);
+        check();
+        three();
+        if (!win) {
+            setTimeout(() => {
+                clearColor();
+            }, 300);
+        }
+    }
+});
+
+bottomRight.addEventListener('click', e => {
+    if (on) {
+        // Sections that user has clicked
+        playerOrder.push(4);
+        check();
+        four();
+        if (!win) {
+            setTimeout(() => {
+                clearColor();
+            }, 300);
+        }
+    }
+});
+
+function check() {
+    // playerOrder.length - 1 is the last thing user clicked on
+    // 'good = false' means user has answered incorrectly
+    if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1]) good = false;
+
+    // 'good' instead of writing 'good = true'
+    if (playerOrder.length == 20 && good) {
+        winGame();
+    }
+
+    if (good == false) {
+        flashColor();
+        turnCounter.innerHTML = "NO!";
+        setTimeout(() => {
+            turnCounter.innerHTML = turn;
+            clearColor();
+
+            if (strict) {
+                // Repeat the game
+                play(); 
+            } else {
+                // Repeat the round
+                compTurn = true;
+                flash = 0;
+                playerOrder = [];
+                good = true;
+                intervalId = setInterval(gameTurn, 800);
+            }
+        }, 800);
+
+        noise = false;
+    }
+
+    if (turn == playerOrder.length && good && !win) {
+        // Next turn..
+        turn++;
+        playerOrder = [];
+        compTurn = true;
+        flash = 0;
+        turnCounter.innerHTML = turn;
+        intervalId = setInterval(gameTurn, 800);
+    }
+}
+
+function winGame() {
+    flashColor();
+    turnCounter.innerHTML = 'Win!';
+    on = false;
+    win = true;
+}
